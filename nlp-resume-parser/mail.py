203b832361c916c44ac1a304c2103c96e12a41c1
st0rmg0d <55540414+st0rmg0d@users.noreply.github.com>
2022-11-09 23:35:16 +0600

    Add files via upload

---
+++ nlp-resume-parser/mail.py
@@ -0,0 +1,8 @@
+import re
+from turtle import textinput
+
+
+def extract_email_addresses(string):
+    r = re.compile(r'[\w\.-]+@[\w\.-]+')
+    return r.findall(string)
+print('Mail id: ',extract_email_addresses(textinput))
---
+++ nlp-resume-parser/name.py
@@ -0,0 +1,26 @@
+from turtle import textinput
+import spacy
+
+from spacy.matcher import Matcher
+
+# load pre-trained model
+nlp = spacy.load('en_core_web_sm')
+
+# initialize matcher with a vocab
+matcher = Matcher(nlp.vocab)
+
+
+def extract_name(resume_text):
+    nlp_text = nlp(resume_text)
+    
+    # First name and Last name are always Proper Nouns
+    pattern = [{'POS': 'PROPN'}, {'POS': 'PROPN'}]
+    
+    matcher.add('NAME', None, pattern)
+    
+    matches = matcher(nlp_text)
+    
+    for match_id, start, end in matches:
+        span = nlp_text[start:end]
+        return span.text
+print('Name: ',extract_name(textinput))
